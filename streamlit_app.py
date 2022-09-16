d32aa5fc3697f14bd0b48202d8397517c306f019
Brandy Cagle <113396872+brandycagle1@users.noreply.github.com>
2022-09-15 19:29:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,24 +25,39 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+# Create the function
+def get_fruityvice_date(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return (fruityvice_normalized)
+  
 # New section to display fruit advice
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
+      streamlit.error("Please select a fruit to get information.")
   else
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # format json 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # display
-    streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
     
-streamlit.write('The user entered ', fruit_choice)
 
-
+streamlit.header("The fruit load list contains:")
+#Snowflake function
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+      my_cur.execute("SELECT * from fruit_load_list")
+      return my_cur.fetchall()
+    
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
+streamlit.stop()
 
 
 
@@ -54,7 +69,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
+
 streamlit.dataframe(my_data_rows)
 
 #Allow the end user to add a fruit to the list
